cd4c73644d323450c0f8d8258a441f02b23d1a24
Mark Turner <mark@amerine.net>
2011-12-19 01:13:50 -0800

    Update login redirect url to use bendbulletin.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     unless current_user
-      redirect_to '/login?domain=amerine.net'
+      redirect_to '/login?domain=bendbulletin.com'
       return false
     end
   end
