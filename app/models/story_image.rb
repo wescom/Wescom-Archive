6af8f3487c0259145d4d58960c77a1fcd23459b4
Steve Hoffmann <shoffmann@bendbulletin.com>
2012-05-26 18:43:32 -0700

    adjusted paperclip image to import tiff and eps

--- app/models/story_image.rb
+++ app/models/story_image.rb
@@ -3,13 +3,13 @@ class StoryImage < ActiveRecord::Base
   
   has_attached_file :image, 
       :styles => { 
-        :thumb  => "100x100>",
-        :medium => "200x200>",
-        :large => "400x400>"
+        :thumb  => ["100x100>",:jpg],
+        :medium => ["200x200>",:jpg],
+        :large => ["400x400>",:jpg]
       },
       :default_url => '/images/no-image.jpg'
 
-  before_post_process :is_image?
+#  before_post_process :is_image?
   def is_image?
     !(image_content_type =~ /^image.*/).nil?
   end
