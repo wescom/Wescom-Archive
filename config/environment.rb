dfdb26295c6b3c74b122a4510c276282283fb123
Steve Hoffmann <shoffmann@bendbulletin.com>
2012-10-18 11:19:44 -0700

    added timezone setting

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-config.time_zone = 'Pacific Time (US & Canada)'
+config.time_zone = 'PST'
 
 # Initialize the rails application
 Wescomarchive::Application.initialize!
