99dbf1d07caac11471ac7511a95400992ccd3d15
Steve Hoffmann <shoffmann@bendbulletin.com>
2012-10-29 17:11:17 -0700

    updated import script to ignore output from bundle install command

--- lib/tasks/daily_import.sh
+++ lib/tasks/daily_import.sh
@@ -5,6 +5,6 @@ source /usr/local/rvm/environments/ruby-1.9.3-p194@rails-3.2.3
 
 echo "$(date +%m/%d/%y\ %T)"
 cd /u/apps/wescomarchive/current
-bundle install
+bundle install>>/tmp/null
 
 rake wescom:dti_import RAILS_ENV=production
