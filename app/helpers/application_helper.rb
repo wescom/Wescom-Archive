3f96ae925a82598bff10cc68aeaca45384a0e851
Steve Hoffmann <shoffmann@bendbulletin.com>
2012-11-08 11:06:54 -0800

    strip all nonalphanumberic characters from story link. it was break the search index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,14 @@ module ApplicationHelper
   def text_for_story_link(story)
     text = if !story.hl1.nil? and story.hl1.length > 0
       story.hl1
-    elsif !story.hl2.nil?
+    elsif !story.hl2.nil? and story.hl2.length > 0
       story.hl2
-    elsif !story.copy.nil?
+    elsif !story.copy.nil? and story.copy.length > 0
       truncate(story.copy.gsub(/<.*?>/, ''), :length => 50)
     else
       story.doc_name
     end
-    text.html_safe
+    # strip all nonalphanumberic characters 
+    text.html_safe.gsub(/\W/, ' ')
   end
 end
