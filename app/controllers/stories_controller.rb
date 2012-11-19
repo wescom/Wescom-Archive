2861b9a7b9c29e7cc3075b16436437838c216a80
Steve Hoffmann <shoffmann@bendbulletin.com>
2012-11-19 15:14:47 -0800

    turned on ability to delete stories from the index listing

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,10 +2,18 @@ class StoriesController < ApplicationController
   before_filter :require_user
   def show
     @story = Story.where(:id => params[:id]).includes(:corrections, :corrected_stories).first
-    if !@story.project_group.nil?
-      @related_stories = Story.where(:project_group => @story.project_group)
+  end
+  
+  def destroy
+    @story = Story.find(params[:id])
+    if @story.destroy
+      flash[:notice] = "Story Killed!"
+      redirect_to :back and return unless request.referrer == story_path(@story)
+      redirect_to search_path
     else
-      @related_stories = nil
+      flash[:error] = "Story Deletion Failed"
+      redirect_to :back and return unless request.referrer == story_path(@story)
+      redirect_to search_path
     end
   end
 end
