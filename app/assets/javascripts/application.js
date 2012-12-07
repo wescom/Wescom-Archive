7867fe2b749c4c766cd7c35c7ca96899e2794b46
Steve Hoffmann <shoffmann@bendbulletin.com>
2012-12-06 16:03:16 -0800

    Todays Stories publication list hides and shows with click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,9 +4,15 @@ $(document).ready(function(){
 	// Advanced Search
   $("#advanced-search").hide();
   $(".short_date_picker").datepicker();
+	$("#sidebar_today").hide();
 
   $(".toggle_hide").click(function () {
     $("#advanced-search").slideToggle("slow");
+    return false;
+  });
+
+  $(".toggle_today").click(function () {
+    $("#sidebar_today").slideToggle("medium");
     return false;
   });
 
