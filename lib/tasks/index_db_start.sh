9431cb780929549345ba3e967b5b465ca0a69332
Steve Hoffmann <shoffmann@bendbulletin.com>
2012-12-12 09:37:11 -0800

    renamed script and created a start index script

---
+++ lib/tasks/index_db_start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# load rvm ruby
+source /usr/local/rvm/environments/ruby-1.9.3-p194
+
+echo "$(date +%m/%d/%y\ %T)"
+cd /u/apps/wescomarchive/current
+bundle install
+
+rake sunspot:solr:start RAILS_ENV=production
