a6ee68969b406351c8c7f283f781866d06547802
Steve Hoffmann <shoffmann@bendbulletin.com>
2012-12-31 12:42:55 -0800

    fixed date search bug

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,8 +9,8 @@ class SearchController < ApplicationController
           paginate(:page => params[:page])
           order_by :pubdate, :desc
           fulltext params[:search_query]
-          with(:pubdate).greater_than(DateTime.strptime(params[:date_from_select], '%y/%d/%m')) if params[:date_from_select].present?
-          with(:pubdate).less_than(DateTime.strptime(params[:date_to_select], '%y/%d/%m')) if params[:date_to_select].present?
+          with(:pubdate).greater_than(Date.strptime(params[:date_from_select], "%m/%d/%Y")) if params[:date_from_select].present?
+          with(:pubdate).less_than(Date.strptime(params[:date_to_select], "%m/%d/%Y")) if params[:date_to_select].present?
 #          facet(:publish_year)
 #          with(:publish_year, params[:year]) if params[:year].present?
         end
