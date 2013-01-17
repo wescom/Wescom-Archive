0e393804d2508d1ed1bd9af52f25dabffc148dec
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-01-16 19:26:56 -0800

    updated syling to be fluid. new 960 fluid grid system and changes to all views.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,137 @@
-
 $(document).ready(function(){
+
+	// Screen Resizing - Hide/Show Table Columns
+	if (parseInt($('#table-block').css("width"))) {
+		//alert('Screen size: ' + parseInt($('#table-block').css("width")));
+		screen_width=parseInt($('#table-block').css("width"))
+		if (screen_width < 578) {
+			$(".action_col").hide();
+		}
+		else {
+			$(".action_col").show();
+		}
+		
+		if (screen_width < 519) {
+			$(".status_col").hide();
+		}
+		else {
+			$(".status_col").show();
+		}
+		
+		if (screen_width < 519) {
+			$(".system_col").hide();
+		}
+		else {
+			$(".system_col").show();
+		}
+		
+		if (screen_width < 409) {
+			$(".section_col").hide();
+		}
+		else {
+			$(".section_col").show();
+		}
+  };
+
+	if (parseInt($('#story-content').css("width"))) {
+		screen_width=parseInt($('#story-content').css("width"))
+		//alert('story-content size: '+screen_width);
+		if (screen_width < 830) {
+			$("#sidebar").hide();
+			$('#story_block').css('width', '95%');
+			$("#lower-block").show();
+		}
+		else {
+			$("#sidebar").show();
+			$('#story_block').css('width', '');
+			$("#lower-block").hide();
+		}
+	};
+
+	if (parseInt($('#image-content').css("width"))) {
+		screen_width=parseInt($('#image-content').css("width"))
+		//alert('story-content size: '+screen_width);
+		if (screen_width < 920) {
+			$("#image_edit").hide();
+			$('#image_image').css('width', '95%');
+			$('#image_info').css('width', '95%');
+		}
+		else {
+			$("#image_edit").show();
+			$('#image_image').css('width', '');
+			$('#image_info').css('width', '');
+		}
+		if (screen_width < 460) {
+			$('.image_large').css('width', '95%');
+		}
+		else {
+			$('.image_large').css('width', '');
+		}
+	};
+
+	$(window).resize(function() {
+		screen_width=parseInt($('#table-block').css("width"))
+		// alert('Screen size: '+screen_width);
+		if (screen_width < 578) {
+			$(".action_col").hide();
+		}
+		else {
+			$(".action_col").show();
+		}
+		
+		if (screen_width < 519) {
+			$(".status_col").hide();
+		}
+		else {
+			$(".status_col").show();
+		}
+		
+		if (screen_width < 519) {
+			$(".system_col").hide();
+		}
+		else {
+			$(".system_col").show();
+		}
+		
+		if (screen_width < 409) {
+			$(".section_col").hide();
+		}
+		else {
+			$(".section_col").show();
+		}
+
+		screen_width=parseInt($('#story-content').css("width"))
+		//alert('story-content size: '+screen_width);
+		if (screen_width < 830) {
+			$("#sidebar").hide();
+			$('#story_block').css('width', '95%');
+			$("#lower-block").show();
+		}
+		else {
+			$("#sidebar").show();
+			$('#story_block').css('width', '');
+			$("#lower-block").hide();
+		}
+
+		screen_width=parseInt($('#image-content').css("width"))
+		//alert('image-content size: '+screen_width);
+		if (screen_width < 920) {
+			$("#image_edit").hide();
+			$('#image_image').css('width', '95%');
+			$('#image_info').css('width', '95%');
+		}
+		else {
+			$("#image_edit").show();
+			$('#image_image').css('width', '');
+			$('#image_info').css('width', '');
+		}
+		if (screen_width < 460) {
+			$('.image_large').css('width', '95%');
+		}
+		else {
+			$('.image_large').css('width', '');
+		}
+  });
 
 	// Advanced Search
   $("#advanced-search").hide();
