00db052b607f0119787a237a1297719e8c7433f1
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-01-28 14:09:31 -0800

    fire masonry loading of images earlier in scroll

--- public/javascripts/story_images.js
+++ public/javascripts/story_images.js
@@ -3,7 +3,7 @@ jQuery(function() {
     $(window).scroll(function() {
       var url;
       url = $('.pagination .next_page').attr('href');
-      if (url && ($(window).scrollTop() > ($(document).height() - $(window).height() - 100))) {
+      if (url && ($(window).scrollTop() > ($(document).height() - $(window).height() - 400))) {
         $('.pagination').text("Fetching more products...");
         return $.getScript(url);
       }
