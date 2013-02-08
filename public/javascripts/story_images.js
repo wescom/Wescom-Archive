7e46134b12d46d68f1e18027bd633a66595a7ae2
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-02-08 08:37:05 -0800

    load story images sooner in image gallery

--- public/javascripts/story_images.js
+++ public/javascripts/story_images.js
@@ -3,7 +3,7 @@ jQuery(function() {
     $(window).scroll(function() {
       var url;
       url = $('.pagination .next_page').attr('href');
-      if (url && ($(window).scrollTop() > ($(document).height() - $(window).height() - 600))) {
+      if (url && ($(window).scrollTop() > ($(document).height() - $(window).height() - 800))) {
 //        $('.pagination').text('Fetching more products...');
 			  $('.pagination').replaceWith('<div class="pagination fetching_images_text"><img class="spinner" src="./images/Spinner.gif"></br>Fetching more images...</div>');
         return $.getScript(url);
