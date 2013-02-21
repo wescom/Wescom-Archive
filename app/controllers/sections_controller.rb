ae94da8b4439d6b092dc8811083efc7d9ca3415a
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-02-21 07:02:04 -0800

    added sections to navbar and sections edit page

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,4 +2,23 @@ class SectionsController < ApplicationController
   def index
     @sections = Section.paginate(:page => params[:page], :per_page => 60, :order=> "name")
   end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+
+  def edit
+    @section = Section.find(params[:id])
+  end
+
+  def update
+    @section = Section.find(params[:id])
+    if @section.update_attributes(params[:section])
+      flash[:notice] = "Section updated"
+      redirect_to sections_url
+    else
+      render :action => :edit
+    end
+  end
+
 end
