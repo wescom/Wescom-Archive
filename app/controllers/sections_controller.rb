9801096d4b44924b947083ade88e9bd70bfe6e3c
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-02-20 20:50:24 -0800

    changed number of sections to show in index view

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,5 +1,5 @@
 class SectionsController < ApplicationController
   def index
-    @sections = Section.paginate(:page => params[:page], :order=> "name")
+    @sections = Section.paginate(:page => params[:page], :per_page => 60, :order=> "name")
   end
 end
