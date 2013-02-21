99a82f59135eaf2085e802293f99126280a0986b
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-02-21 11:34:30 -0800

    added category_plus_name to sections

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,6 +2,10 @@ class Section < ActiveRecord::Base
   has_many :stories
 
   def category_plus_name
-    category+" - "+name
+    if category?
+      category+" - "+name
+    else
+      name
+    end
   end
 end
