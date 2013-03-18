2568c98d5506b41224fb5200a24b74f57127c37b
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-03-18 13:40:00 -0700

    pass publication when linking from story to pdf_images

--- app/controllers/pdf_images_controller.rb
+++ app/controllers/pdf_images_controller.rb
@@ -10,6 +10,9 @@ class PdfImagesController < ApplicationController
     if (params[:date_to_select].present?)
       scope = scope.where('DATE(pubdate) <= ?', Date.strptime(params[:date_to_select], "%m/%d/%Y"))
     end
+    if (params[:publication].present?)
+      scope = scope.where('publication = ?', params[:publication])
+    end
     if (params[:sectionletter].present?)
       scope = scope.where('section_letter = ?', params[:sectionletter])
     end
