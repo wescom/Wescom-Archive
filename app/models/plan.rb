5fe95a90261bbef26ca6180c71103d635570ddf4
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-04-08 13:54:26 -0700

    extended the sort order of the plan index to include the imported fields

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,6 +4,6 @@ class Plan < ActiveRecord::Base
   belongs_to :publication_type
 
   def self.order_by_location_type_pub_section
-    includes([:location, :publication_type]).order('locations.name').order('publication_types.name').order('pub_name').order('section_name')
+    includes([:location, :publication_type]).order('locations.name').order('publication_types.name').order('pub_name').order('section_name').order('import_pub_name').order('import_section_name')
   end
 end
