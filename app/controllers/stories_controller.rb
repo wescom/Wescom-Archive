f32357da6c73f2e06baf5b7812efc9bd601a56ca
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-04-09 14:18:13 -0700

    fixing bug display story with no paper name

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,7 +3,7 @@ class StoriesController < ApplicationController
 
   def show
     @story = Story.where(:id => params[:id]).includes(:corrections, :corrected_stories).first
-    pub = (@story.publication.name == "The Bulletin" ? "The Bulletin" : "Redmond Spokesman")
+    #pub = (@story.publication.name == "The Bulletin" ? "The Bulletin" : "Redmond Spokesman")
     @pdf_image = PdfImage.where(:pubdate=>@story.pubdate).where(:publication=>pub).order_by_pubdate_section_page.first
 
     respond_to do |format|
