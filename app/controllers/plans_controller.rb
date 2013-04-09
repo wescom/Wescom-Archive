85b32c21b9c0d1e5a98a15d09c4424819eab257c
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-04-09 07:43:06 -0700

    moved sample plan links from index to edit view

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -11,12 +11,16 @@ class PlansController < ApplicationController
     @plan = Plan.find(params[:id])
     @locations = Location.find(:all, :order => "name")
     @publication_types = PublicationType.find(:all, :order => "sort_order")
+    @pdfs = @plan.pdf_images.limit(3)
+    @stories = @plan.stories.limit(3)
   end
 
   def update
     @plan = Plan.find(params[:id])
     @locations = Location.find(:all, :order => "name")
     @publication_types = PublicationType.find(:all, :order => "sort_order")
+    @pdfs = @plan.pdf_images.limit(3)
+    @stories = @plan.stories.limit(3)
     if params[:cancel_button]
       redirect_to plans_path
     else
