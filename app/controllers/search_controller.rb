1d1f721588c414ec34eee01cc5496e08cf3608fa
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-05-01 10:57:05 -0700

    reset section selection list based on location and pub_type after a search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,7 +11,11 @@ class SearchController < ApplicationController
     @publications = @publications.where(:publication_type_id => params[:pub_type]) if params[:pub_type].present?
     @publications = @publications.select(:pub_name).uniq.order('pub_name')
 
-    @sections = Plan.select(:section_name).where("section_name is not null and section_name<>''").uniq.order('section_name')
+    @sections = Plan.where("section_name is not null and section_name<>''")
+    @sections = @sections.where(:location_id => params[:location]) if params[:location].present?
+    @sections = @sections.where(:publication_type_id => params[:pub_type]) if params[:pub_type].present?
+    @sections = @sections.where(:pub_name => params[:pub_select]) if params[:pub_select].present?
+    @sections = @sections.select(:section_name).uniq.order('section_name')
 
     if params[:search_query]
       begin
--- app/controllers/story_images_controller.rb
+++ app/controllers/story_images_controller.rb
@@ -7,7 +7,11 @@ class StoryImagesController < ApplicationController
     @publications = @publications.where(:publication_type_id => params[:pub_type]) if params[:pub_type].present?
     @publications = @publications.select(:pub_name).uniq.order('pub_name')
 
-    @sections = Plan.select(:section_name).where("section_name is not null and section_name<>''").uniq.order('section_name')
+    @sections = Plan.where("section_name is not null and section_name<>''")
+    @sections = @sections.where(:location_id => params[:location]) if params[:location].present?
+    @sections = @sections.where(:publication_type_id => params[:pub_type]) if params[:pub_type].present?
+    @sections = @sections.where(:pub_name => params[:pub_select]) if params[:pub_select].present?
+    @sections = @sections.select(:section_name).uniq.order('section_name')
 
     scope = StoryImage
     scope = scope.has_pubdate_in_range(params[:date_from_select], params[:date_to_select])
@@ -27,7 +31,12 @@ class StoryImagesController < ApplicationController
     @publications = @publications.where(:publication_type_id => params[:pub_type]) if params[:pub_type].present?
     @publications = @publications.select(:pub_name).uniq.order('pub_name')
 
-    @sections = Plan.select(:section_name).where("section_name is not null and section_name<>''").uniq.order('section_name')
+    @sections = Plan.where("section_name is not null and section_name<>''")
+    @sections = @sections.where(:location_id => params[:location]) if params[:location].present?
+    @sections = @sections.where(:publication_type_id => params[:pub_type]) if params[:pub_type].present?
+    @sections = @sections.where(:pub_name => params[:pub_select]) if params[:pub_select].present?
+    @sections = @sections.select(:section_name).uniq.order('section_name')
+
     if params[:search_query]
       begin
         @images = StoryImage.search(:include => [:story]) do
