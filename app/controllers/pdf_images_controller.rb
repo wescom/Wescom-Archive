66202dacfaa76d6735178ea615531829c1cf676e
Steve Hoffmann <shoffmann@bendbulletin.com>
2015-03-15 17:38:56 -0700

    get pdf page number from image filename being uploaded

--- app/controllers/pdf_images_controller.rb
+++ app/controllers/pdf_images_controller.rb
@@ -101,12 +101,20 @@ class PdfImagesController < ApplicationController
       @pdf_images = params[:pdf_image][:image]
       # Loop through each pdf file and save to database
       for i in (0..@pdf_images.length-1)
+        # set pdf_image to current image in loop
         params[:pdf_image][:image] = @pdf_images[i]
   
         @pdf_image = PdfImage.new(params[:pdf_image])
+        
+        # Assign plan to this pdf_image
         @plan = Plan.find_or_create_by_location_id_and_publication_type_id_and_pub_name_and_section_name_and_import_section_letter(
                       params[:location],params[:pub_type],@pdf_image.publication,@pdf_image.section_name,@pdf_image.section_letter)
         @pdf_image.plan_id = @plan.id
+
+        # Get page number from pdf filename
+        @section_page = @pdf_image.image_file_name.match(/[a-zA-Z]\d{1,3}/).to_s
+        @page = @section_page.match(/\d{1,3}/).to_s
+        @pdf_image.page = @page
 
         if @pdf_image.save
           flash[:notice] = "PDF Created"
