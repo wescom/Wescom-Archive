7f13f6e9616e83eade8043a90aa68c07365f331e
Steve Hoffmann <shoffmann@bendbulletin.com>
2016-11-22 15:51:59 -0800

    added script to call wescom:import_google_pdf. script access paramter for number of records to import

--- lib/tasks/google_pdf_import.sh
+++ lib/tasks/google_pdf_import.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#
+# Script to import Google PDF pages
+#   ie. google_pdf_import.sh <number of records to import>
+#   If the parameter is not included, it is assumed to only import one record
+#
+
 # load rvm ruby
 source /usr/local/rvm/environments/ruby-1.9.3-p194
 
@@ -8,4 +14,10 @@ cd /u/apps/wescomarchive/current
 bundle install>>/tmp/null
 
 echo "Import Google PDF Files"
-bundle exec rake wescom:import_google_pdf RAILS_ENV=production
+
+if [ -z "$1" ]
+then
+	bundle exec rake wescom:import_google_pdf[1] RAILS_ENV=production
+else
+	bundle exec rake wescom:import_google_pdf[$1] RAILS_ENV=production
+fi
