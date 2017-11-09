042ef204b94a508a47a81e1aa32b596eae13e167
Steve Hoffmann <shoffmann@bendbulletin.com>
2017-11-09 07:38:17 -0800

    added more indexes to pdf_images table

---
+++ db/migrate/20171109152806_add_index_to_pdf_images.rb
@@ -0,0 +1,11 @@
+class AddIndexToPdfImages < ActiveRecord::Migration
+  def up
+    add_index :pdf_images, [:section_letter, :page], :name => 'letter_page'
+    add_index :pdf_images, [:pubdate, :section_letter, :page], :name => 'date_letter_page'
+  end
+
+  def down
+    remove_index :pdf_images, [:section_letter, :page], :name => 'letter_page'
+    remove_index :pdf_images, [:pubdate, :section_letter, :page], :name => 'date_letter_page'
+  end
+end
