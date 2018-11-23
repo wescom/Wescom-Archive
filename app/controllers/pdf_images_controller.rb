93c0ff1f926565816e751885cab75d9641b06848
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-11-23 10:07:55 -0800

    got pdf uploads to work with one section

--- app/controllers/pdf_images_controller.rb
+++ app/controllers/pdf_images_controller.rb
@@ -99,227 +99,67 @@ class PdfImagesController < ApplicationController
 
   def create
     if params[:cancel_button]
-      redirect_to pdf_images_path(:date_from_select=>params[:date_from_select], :date_to_select=>params[:date_to_select], 
-          :location=>params[:location], :pub_type=>params[:pub_type], :pub_select=>params[:pub_select], 
-          :sectionletter=>params[:sectionletter], :pagenum=>params[:pagenum])
-    else
-      section1_success = true
-      section2_success = true
-      section3_success = true
-      section4_success = true
-
-      ## Upload Section1 PDFs
-      @pdf_images = params[:pdf_image][:image1]
-      # Loop through each pdf file and save to database
-      if @pdf_images.present?
-        for i in (0..@pdf_images.length-1)
-          # set pdf_image to current image in loop
-          params[:pdf_image][:image] = @pdf_images[i]
-
-          @pdf_image = PdfImage.new(pdf_image_params)
-
-          # Assign plan to this pdf_image
-#          @plan = Plan.find_or_create_by_location_id_and_publication_type_id_and_pub_name_and_section_name_and_import_section_letter(
-#                        params[:location],params[:pub_type],@pdf_image.publication,@pdf_image.section_name1,@pdf_image.section_letter1)
-          @plan = Plan.find_or_create_by(
-                        location_id: params[:location],
-                        publication_type_id: arams[:pub_type],
-                        pub_name: @pdf_image.publication,
-                        section_name: @pdf_image.section_name1,
-                        import_section_letter: @pdf_image.section_letter1)
-          @pdf_image.plan_id = @plan.id
-
-          # Get page number from pdf filename
-          @section_page = @pdf_image.image_file_name.match(/[a-zA-Z]\d{1,3}/).to_s
-          @page = @section_page.match(/\d{1,3}/).to_s
-          @pdf_image.page = @page
-
-          @pdf_image.section_name = params[:pdf_image][:section_name1]
-          @pdf_image.section_letter = params[:pdf_image][:section_letter1]
-        
-          if @pdf_image.save
-            # Read text within PDF file to use for fulltext indexing/searching
-            yomu = Yomu.new @pdf_image.image.path
-            pdftext = yomu.text
-            pdftext.gsub! /\n/, " "               # Clear newlines
-            pdftext.gsub! "- ", ""                # Clear hyphens from justication
-            @pdf_image.pdf_text = pdftext
-
-            if @pdf_image.save
-              Log.create_log("Pdf_image",@pdf_image.id,"Uploaded","PDF uploaded",current_user)
-              flash[:notice] = "PDFs Uploaded"
-              section1_success = true
-            else
-              Rails.logger.info "*** pdf_image failed save"
-              flash[:error] = "PDF Upload Failed"
-              section1_success = false
-            end
-          else
-            Rails.logger.info "*** pdf_image failed save"
-            flash[:error] = "PDF Upload Failed"
-            section1_success = false
-          end
-        end
-      else
-        Rails.logger.info "*** pdf_images not present"
-        flash[:error] = "PDF Upload Failed"
-        section1_success = false
-      end
-
-      ## Upload Section2 PDFs
-      @pdf_images = params[:pdf_image][:image2]
-      # Loop through each pdf file and save to database
-      if @pdf_images.present?
-        for i in (0..@pdf_images.length-1)
-          # set pdf_image to current image in loop
-          params[:pdf_image][:image] = @pdf_images[i]
-
-          @pdf_image = PdfImage.new(pdf_image_params)
-
-          # Assign plan to this pdf_image
-          @plan = Plan.find_or_create_by_location_id_and_publication_type_id_and_pub_name_and_section_name_and_import_section_letter(
-                        params[:location],params[:pub_type],@pdf_image.publication,@pdf_image.section_name2,@pdf_image.section_letter2)
-          @pdf_image.plan_id = @plan.id
-
-          # Get page number from pdf filename
-          @section_page = @pdf_image.image_file_name.match(/[a-zA-Z]\d{1,3}/).to_s
-          @page = @section_page.match(/\d{1,3}/).to_s
-          @pdf_image.page = @page
-
-          @pdf_image.section_name = params[:pdf_image][:section_name2]
-          @pdf_image.section_letter = params[:pdf_image][:section_letter2]
-
-          if @pdf_image.save
-            # Read text within PDF file to use for fulltext indexing/searching
-            yomu = Yomu.new @pdf_image.image.path
-            pdftext = yomu.text
-            pdftext.gsub! /\n/, " "               # Clear newlines
-            pdftext.gsub! "- ", ""                # Clear hyphens from justication
-            @pdf_image.pdf_text = pdftext
-
-            if @pdf_image.save
-              Log.create_log("Pdf_image",@pdf_image.id,"Uploaded","PDF uploaded",current_user)
-              flash[:notice] = "PDFs Uploaded"
-              section2_success = true
-            else
-              Rails.logger.info "*** pdf_image failed save"
-              flash[:error] = "PDF Upload Failed"
-              section2_success = false
-            end
-          else
-            Rails.logger.info "*** pdf_image failed save"
-            flash[:error] = "PDF Upload Failed"
-            section2_success = false
-          end
-        end
-      end
-
-      ## Upload Section3 PDFs
-      @pdf_images = params[:pdf_image][:image3]
-      # Loop through each pdf file and save to database
-      if @pdf_images.present?
-        for i in (0..@pdf_images.length-1)
-          # set pdf_image to current image in loop
-          params[:pdf_image][:image] = @pdf_images[i]
-
-          @pdf_image = PdfImage.new(pdf_image_params)
-
-          # Assign plan to this pdf_image
-          @plan = Plan.find_or_create_by_location_id_and_publication_type_id_and_pub_name_and_section_name_and_import_section_letter(
-                        params[:location],params[:pub_type],@pdf_image.publication,@pdf_image.section_name3,@pdf_image.section_letter3)
-          @pdf_image.plan_id = @plan.id
-
-          # Get page number from pdf filename
-          @section_page = @pdf_image.image_file_name.match(/[a-zA-Z]\d{1,3}/).to_s
-          @page = @section_page.match(/\d{1,3}/).to_s
-          @pdf_image.page = @page
-
-          @pdf_image.section_name = params[:pdf_image][:section_name3]
-          @pdf_image.section_letter = params[:pdf_image][:section_letter3]
-
-          if @pdf_image.save
-            # Read text within PDF file to use for fulltext indexing/searching
-            yomu = Yomu.new @pdf_image.image.path
-            pdftext = yomu.text
-            pdftext.gsub! /\n/, " "               # Clear newlines
-            pdftext.gsub! "- ", ""                # Clear hyphens from justication
-            @pdf_image.pdf_text = pdftext
-
-            if @pdf_image.save
-              Log.create_log("Pdf_image",@pdf_image.id,"Uploaded","PDF uploaded",current_user)
-              flash[:notice] = "PDFs Uploaded"
-              section3_success = true
-            else
-              Rails.logger.info "*** pdf_image failed save"
-              flash[:error] = "PDF Upload Failed"
-              section3_success = false
-            end
-          else
-            Rails.logger.info "*** pdf_image failed save"
-            flash[:error] = "PDF Upload Failed"
-            section3_success = false
-          end
-        end
-      end
-      
-      ## Upload Section4 PDFs
-      @pdf_images = params[:pdf_image][:image4]
-      # Loop through each pdf file and save to database
-      if @pdf_images.present?
-        for i in (0..@pdf_images.length-1)
-          # set pdf_image to current image in loop
-          params[:pdf_image][:image] = @pdf_images[i]
-
-          @pdf_image = PdfImage.new(pdf_image_params)
-
-          # Assign plan to this pdf_image
-          @plan = Plan.find_or_create_by_location_id_and_publication_type_id_and_pub_name_and_section_name_and_import_section_letter(
-                        params[:location],params[:pub_type],@pdf_image.publication,@pdf_image.section_name4,@pdf_image.section_letter4)
-          @pdf_image.plan_id = @plan.id
-
-          # Get page number from pdf filename
-          @section_page = @pdf_image.image_file_name.match(/[a-zA-Z]\d{1,3}/).to_s
-          @page = @section_page.match(/\d{1,3}/).to_s
-          @pdf_image.page = @page
-
-          @pdf_image.section_name = params[:pdf_image][:section_name4]
-          @pdf_image.section_letter = params[:pdf_image][:section_letter4]
-
-          if @pdf_image.save
-            # Read text within PDF file to use for fulltext indexing/searching
-            yomu = Yomu.new @pdf_image.image.path
-            pdftext = yomu.text
-            pdftext.gsub! /\n/, " "               # Clear newlines
-            pdftext.gsub! "- ", ""                # Clear hyphens from justication
-            @pdf_image.pdf_text = pdftext
-
-            if @pdf_image.save
-              Log.create_log("Pdf_image",@pdf_image.id,"Uploaded","PDF uploaded",current_user)
-              flash[:notice] = "PDFs Uploaded"
-              section4_success = true
-            else
-              Rails.logger.info "*** pdf_image failed save"
-              flash[:error] = "PDF Upload Failed"
-              section4_success = false
-            end
-          else
-            Rails.logger.info "*** pdf_image failed save"
-            flash[:error] = "PDF Upload Failed"
-            section4_success = false
-          end
-        end
-      end
-
-      if section1_success and section2_success and section3_success and section4_success
         redirect_to pdf_images_path(:date_from_select=>params[:date_from_select], :date_to_select=>params[:date_to_select], 
             :location=>params[:location], :pub_type=>params[:pub_type], :pub_select=>params[:pub_select], 
             :sectionletter=>params[:sectionletter], :pagenum=>params[:pagenum])
-      else
-        @locations = Location.all.order('name')
-        @pub_types = PublicationType.all.order('sort_order')
-        render :action => :new
-#        redirect_to new_pdf_image_path(:pdf_image => params[:pdf_image])
-      end
+    else
+        ## Upload Section PDFs
+        @pdf_images = params[:pdf_image][:image]
+        # Loop through each pdf file and save to database
+        if @pdf_images.present?
+            for i in (0..@pdf_images.length-1)
+                # set pdf_image to current image in loop
+                params[:pdf_image][:image] = @pdf_images[i]
+
+                # Assign plan to this pdf_image
+                @plan = Plan.find_or_create_by(
+                            location_id: params[:location],
+                            publication_type_id: params[:pub_type],
+                            pub_name: params[:publication],
+                            section_name: params[:pdf_image][:section_name],
+                            import_section_letter: params[:pdf_image][:section_letter])
+                params[:pdf_image][:plan_id] = @plan.id
+
+                # Get page number from pdf filename
+                @section_page = params[:pdf_image][:image].original_filename.match(/[a-zA-Z]\d{1,3}/).to_s
+                @page = @section_page.match(/\d{1,3}/).to_s
+                params[:pdf_image][:page] = @page
+
+                @pdf_image = PdfImage.new(pdf_image_params)
+                if @pdf_image.save
+                    # Read text within PDF file to use for fulltext indexing/searching
+                    yomu = Yomu.new @pdf_image.image.path
+                    pdftext = yomu.text
+                    pdftext.gsub! /\n/, " "               # Clear newlines
+                    pdftext.gsub! "- ", ""                # Clear hyphens from justication
+                    @pdf_image.pdf_text = pdftext
+
+                    if @pdf_image.save
+                        Log.create_log("Pdf_image",@pdf_image.id,"Uploaded","Section 1 PDFs uploaded",current_user)
+                        flash_message :notice, "Section PDF Uploaded: "+@pdf_image.section_name.to_s+" "+@pdf_image.section_letter.to_s+@pdf_image.page.to_s
+                        section1_success = true
+                    else
+                        Rails.logger.info "*** pdf_image failed save"
+                        flash_message :error, "PDF Upload Failed"
+                        section1_success = false
+                    end
+                else
+                    Rails.logger.info "*** pdf_image failed save"
+                    flash_message :error, "PDF Upload Failed"
+                    section1_success = false
+                end
+            end
+        end
+        if section1_success
+            redirect_to pdf_images_path(:date_from_select=>@pdf_image.pubdate.strftime('%m/%d/%Y').to_s, 
+                :date_to_select=>@pdf_image.pubdate.strftime('%m/%d/%Y').to_s, 
+                :location=>params[:location], :pub_type=>params[:pub_type], :pub_select=>params[:pub_select], 
+                :sectionletter=>params[:sectionletter], :pagenum=>params[:pagenum])
+        else
+          @locations = Location.all.order('name')
+          @pub_types = PublicationType.all.order('sort_order')
+          render :action => :new
+        end
     end
   end
   
@@ -376,7 +216,38 @@ class PdfImagesController < ApplicationController
   
   private
     def pdf_image_params
-      params.require(:pdf_image).permit(:plan_id, :location, :pub_type, :publication, :pubdate, :section_name, :section_letter,
-        :section_name, :section_letter, :image)
+      params.require(:pdf_image).permit(:plan_id, :location, :pub_type, :publication, :pubdate, :section_name, :section_letter, :page, :image)
+    end
+    
+    def validate_section1_upload
+      if (params[:section_name1].present? or params[:section_letter1].present? or params[:image1].present?) and (params[:section_name1].empty? or params[:section_letter1].empty? or params[:image1].empty?)
+        flash_message :error, "Section 1 missing data for upload"
+        return false
+      end
+      return true
+    end
+
+    def validate_section2_upload
+        if (params[:section_name2].present? or params[:section_letter2].present? or params[:image2].present?) and (params[:section_name2].empty? or params[:section_letter2].empty? or params[:image2].empty?)
+          flash_message :error, "Section 2 missing data for upload"
+          return false
+        end
+        return true
+    end
+
+    def validate_section3_upload
+        if (params[:section_name3].present? or params[:section_letter3].present? or params[:image3].present?) and (params[:section_name3].empty? or params[:section_letter3].empty? or params[:image3].empty?)
+          flash_message :error, "Section 3 missing data for upload"
+          return false
+        end
+        return true
+    end
+
+    def validate_section4_upload
+        if (params[:section_name4].present? or params[:section_letter4].present? or params[:image4].present?) and (params[:section_name4].empty? or params[:section_letter4].empty? or params[:image4].empty?)
+          flash_message :error, "Section 4 missing data for upload"
+          return false
+        end
+        return true
     end
 end
