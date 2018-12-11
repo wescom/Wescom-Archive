209664e407ca91b99b73f444cd8de2f2ac060b5d
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-12-11 09:51:24 -0800

    update deploy code for rails5 and new servers

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,121 +1,136 @@
-set :application, "wescomarchive"
-set :repository,  "git@github.com:wescom/Wescom-Archive.git"
-set :ssh_options, { :forward_agent => true }
+# config valid only for current version of Capistrano
+# lock "3.7.1"
+
 set :rails_env,   "production"
-set :scm,         :git
-set :scm_username,    "wescomarchive"     # Git user
-set :scm_passphrase,  "Go2cmdarchive"  # Git password
-set :branch,      "origin/master"
-#set :deploy_via,  :remote_cache
-#set :deploy_via,  :copy
 
-set :user,      "shoffmann"  # The server's user for deploys
-set :group,     "archive"
+set :migration_role, :app
 
-require "bundler/capistrano"
-require "rvm/capistrano"
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-set :rvm_ruby_string, 'ruby-1.9.3-p194'
-set :rvm_bin_path, "/usr/local/rvm/bin"
-#set :rvm_type, :system
-set :rvm_install_ruby, :install
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-default_environment["RAILS_ENV"] = 'production'
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
-default_run_options[:pty] =   true
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/application.yml"
 
-#ARCHIVE1 = "216.228.165.100"
-#ARCHIVE2 = "216.228.165.101"
-#ARCHIVE3 = "216.228.165.102"
-ARCHIVE1 = "archive1.wescompapers.com"
-ARCHIVE2 = "archive2.wescompapers.com"
-ARCHIVE3 = "archive3.wescompapers.com"
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+##### Need change to your own configs #####
+ARCHIVE1 = "192.168.112.109"
+ARCHIVE2 = "192.168.112.87"
+ARCHIVE3 = "192.168.112.93"
+#ARCHIVE1 = "archive1.wescompapers.com"
+#ARCHIVE2 = "archive2.wescompapers.com"
+#ARCHIVE3 = "archive3.wescompapers.com"
 role :web, ARCHIVE1
 role :app, ARCHIVE1
 role :app, ARCHIVE2, :solr => true
 role :db,  ARCHIVE3, :primary => true
+#server 'wescomarchive.com', port: 22, roles: [:web, :app, :db], primary: true
 
-namespace :deploy do
-  desc "Deploy Wescom Archive"
-  task :default do
-    update
-    restart
-  end
+set :repo_url, "git@github.com:wescom/Wescom-Archive.git"
+set :application, "wescomarchive"
 
-  task :update do
-    transaction do
-      update_code
+# Settings for Git
+set :scm_username,    "wescomarchive"     # Git user
+set :scm_passphrase,  "Go2cmdarchive"     # Git password
+
+set :puma_threads,    [4, 16]
+set :puma_workers,    2
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/u/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to true if using ActiveRecord
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
 
-  desc "Update deployed code"
-  task :update_code do
-    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
-    finalize_update
-  end
+  before :start, :make_dirs
+end
 
-  desc "Finialize update"
-  task :finalize_update do
-    run <<-CMD
-      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
-      mkdir -p #{latest_release}/public &&
-      mkdir -p #{latest_release}/tmp &&
-      ln -s #{shared_path}/system #{latest_release}/public/system &&
-      ln -s #{shared_path}/log #{latest_release}/log &&
-      ln -s #{shared_path}/pids #{latest_release}/tmp/pids
-    CMD
-    run <<-CMD
-      rm -rf #{latest_release}/solr &&
-      ln -s /WescomArchive/solr #{latest_release}/solr
-    CMD
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
   end
-
-  desc "Zero-downtime restart of Unicorn"
-  task :restart, :roles => :web do
-    run "sudo kill -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
-  end
-
-  desc "Start unicorn"
-  task :start, :roles => :web do
-    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
-  end
-
-  desc "Stop unicorn"
-  task :stop, :roled => :web do
-    run "kill -s QUIT `cat #{shared_path}/pids/unicorn.pid`"
-  end 
   
-  task :ln_assets do
-    run <<-CMD
-      rm -rf #{latest_release}/public/assets &&
-      mkdir -p #{shared_path}/assets &&
-      ln -s #{shared_path}/assets #{latest_release}/public/assets
-    CMD
+  desc "Upload files not included in GitHub repository due to security"
+  before :deploy, :upload_files do
+    on roles(:web) do
+      execute "mkdir -p #{shared_path}/config"
+      upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
+      upload! StringIO.new(File.read("config/application.yml")), "#{shared_path}/config/application.yml"
+    end
   end
 
-#  task :assets do
-#    update_code
-#    ln_assets
+  desc "Update links"
+  after :finished, :update_links do
+    on roles(:web) do
+      execute "rm -rf #{release_path}/solr #{release_path}/log #{release_path}/public/system #{release_path}/tmp/pids"
+      execute "ln -s /WescomArchive/solr #{release_path}/solr"
+      execute "mkdir -p #{release_path}/public && ln -s #{shared_path}/system #{release_path}/public/system"
+      #execute "mkdir -p #{shared_path}/system && ln -s /WescomArchive/db_images #{shared_path}/system/db_images && ln -s /WescomArchive/pdf_images #{shared_path}/system/pdf_images"
+      execute "ln -s #{shared_path}/log #{release_path}/log"
+    end
+  end
+  
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
 
-#    run_locally "rake assets:precompile"
-#    run_locally "cd public; tar -zcvf assets.tar.gz assets"
-#    top.upload "public/assets.tar.gz", "#{shared_path}", :via => :scp
-#    run "cd #{shared_path}; tar -zxvf assets.tar.gz"
-#    run_locally "rm public/assets.tar.gz"
-#    run_locally "rm -rf public/assets"
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+#      Rake::Task["puma:restart"].reenable
+#      invoke 'puma:restart'
+      invoke 'puma:stop'
+      invoke 'puma:start'
+    end
+  end
 
-#    create_symlink
-#    restart
-#  end
-
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 end
 
-#after 'deploy:update_code' do
-#  run "cd #{current_path}; bundle install"
-#  run_rake "assets:precompile"
-#end
-
-def run_rake(cmd)
-  run "cd #{current_path}; #{rake} #{cmd}"
-end
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
