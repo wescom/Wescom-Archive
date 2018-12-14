78d3404fdc442791d33833915ec4806034c1b85c
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-12-14 09:50:25 -0800

    fix deploy issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@
 set :rails_env,   "production"
 
 set :migration_role, :app
-set :assets_role, [:web, :app, :worker]
+#set :assets_role, [:web, :app, :worker]
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -59,8 +59,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-#set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to true if using ActiveRecord
