16cc80ae6ccdd6c63b621652fddb98f83b0aea83
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-12-20 07:26:20 -0800

    switching solr pointer to arcvhive2

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,7 +87,7 @@ namespace :deploy do
   
   desc "Upload files not included in GitHub repository due to security"
   before :deploy, :upload_files do
-    on roles(:web) do
+    on roles(:all) do
       execute "mkdir -p #{shared_path}/config"
       upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
       upload! StringIO.new(File.read("config/application.yml")), "#{shared_path}/config/application.yml"
