42512b80029bf4af60299af9fac8b757df7ada4e
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-12-20 15:39:37 -0800

    fixing puma for development

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,15 +52,18 @@ set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/u/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-#set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-#set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 #set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-# 
+set :puma_threads,    [4, 16]
+set :puma_workers,    2
+
+
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
