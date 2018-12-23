c4f32ed5e8d78a04ec442c68a80a55715da717df
Steve Hoffmann <shoffmann@bendbulletin.com>
2018-12-22 22:34:33 -0800

    move puma settings into puma.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,17 +51,17 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/u/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+#set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+#set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+#set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+#set :puma_access_log, "#{release_path}/log/puma.error.log"
+#set :puma_error_log,  "#{release_path}/log/puma.access.log"
 #set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :puma_threads,    [4, 16]
-set :puma_workers,    2
+#set :puma_threads,    [4, 16]
+#set :puma_workers,    2
 
 
 namespace :puma do
--- config/puma.rb
+++ config/puma.rb
@@ -33,23 +33,29 @@ environment ENV.fetch("RAILS_ENV") { "production" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
+# Change to match your CPU core count
+workers 2
+
+# Min and Max threads per worker
+threads 4, 16
+
 #app_dir = File.expand_path("../..", __FILE__)
 #shared_dir = "#{app_dir}/shared"
 
 # Default to production
-#rails_env = ENV['RAILS_ENV'] || "production"
-#environment rails_env
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
 
 # Set up socket location
-#bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 
 # Logging
-#stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-#pidfile "#{shared_dir}/pids/puma.pid"
-#state_path "#{shared_dir}/pids/puma.state"
-#activate_control_app
+pidfile "#{shared_path}/tmp/pids/puma.pid"
+state_path "#{shared_path}/tmp/pids/puma.state"
+activate_control_app
 
 on_worker_boot do
   require "active_record"
