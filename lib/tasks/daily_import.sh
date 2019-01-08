b99cd75e949295bc3b0a16a30cc06dd57e7a765d
Steve Hoffmann <shoffmann@bendbulletin.com>
2019-01-08 15:03:20 -0800

    updated daily upload script

--- lib/tasks/daily_import.sh
+++ lib/tasks/daily_import.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # load rvm ruby
-source /usr/local/rvm/environments/ruby-1.9.3-p194@rails-3.2.11
+#source /usr/local/rvm/environments/ruby-1.9.3-p194@rails-3.2.11
 
 echo "$(date +%m/%d/%y\ %T)"
 cd /u/apps/wescomarchive/current
 bundle install>>/tmp/null
 
 echo "Import Daily Stories"
-bundle exec rake wescom:dti_import RAILS_ENV=production
+#bundle exec rake wescom:dti_import RAILS_ENV=production
 bundle exec rake wescom:cloud_dti_import RAILS_ENV=production
 
 # force all imported images to new owner, otherwise web process cannot modify/delete
