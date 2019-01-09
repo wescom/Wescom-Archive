262ef890fe15fb4e93129446cf5efce5d0e8ab6f
Steve Hoffmann <shoffmann@bendbulletin.com>
2019-01-09 15:46:45 -0800

    trying to get multiplepuma workers

--- config/puma.rb
+++ config/puma.rb
@@ -4,15 +4,13 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
+#threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+#threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
 port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-#
 environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the number of `workers` to boot in clustered mode.
@@ -51,7 +49,9 @@ environment rails_env
 bind "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+#stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 
 # Set master PID and state locations
 pidfile "#{shared_path}/tmp/pids/puma.pid"
