433944911720ff7ed129fa6f7c01aae71f4aac88
Steve Hoffmann <shoffmann@bendbulletin.com>
2019-01-16 20:10:36 -0800

    altered cron scripts to export PATH prior to importing. ImageMagick was failing due to cron not having same root user PATH as running commands from a terminal session

--- lib/tasks/daily_import_pdfs.sh
+++ lib/tasks/daily_import_pdfs.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+export PATH=/usr/local/rvm/gems/ruby-2.5.1@wescomarchive/bin:/usr/local/rvm/gems/ruby-2.5.1@global/bin:/usr/local/rvm/rubies/ruby-2.5.1/bin:/usr/local/rvm/gems/ruby-2.5.1@wescomarchive/bin:/usr/local/rvm/gems/ruby-2.5.1@global/bin:/usr/local/rvm/rubies/ruby-2.5.1/bin:/usr/local/rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 
 # load rvm ruby
 source /usr/local/rvm/environments/ruby-2.5.1@wescomarchive
